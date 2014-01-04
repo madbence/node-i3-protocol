0fe1f1b55fead9c5f97a4e28b97dc97d527e5e5a
Dányi Bence <madbence@gmail.com>
2014-01-04 16:20:36 +0100

    Test message type param

--- test/encode.js
+++ test/encode.js
@@ -4,8 +4,16 @@ var should = require('should');
 describe('i3p', function() {
   describe('#encode', function() {
     it('should return `Buffer`', function() {
-      var b = i3p.encode();
+      var b = i3p.encode(0);
       b.should.be.a.Buffer;
+    });
+    it('should expect `type` parameter', function() {
+      (function() {
+        var b = i3p.encode(0);
+      }).should.not.throw();
+      (function() {
+        var b = i3p.encode();
+      }).should.throw();
     });
   });
 });
