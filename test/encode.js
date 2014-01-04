f2ecc6bdbb0289b7003fff2a01148a4a2ea64d41
Dányi Bence <madbence@gmail.com>
2014-01-04 16:33:00 +0100

    Test message type

--- test/encode.js
+++ test/encode.js
@@ -11,6 +11,12 @@ describe('i3p', function() {
       var b = i3p.encode(0);
       b.toString().should.startWith('i3-ipc');
     });
+    it('should set the correct type for the message', function() {
+      var b1 = i3p.encode(0);
+      var b2 = i3p.encode(1);
+      b1.readUInt32BE(10).should.equal(0);
+      b2.readUInt32BE(10).should.equal(1);
+    });
     it('should expect `type` parameter', function() {
       (function() {
         var b = i3p.encode(0);
