7a24ed571a4111d8aa8f3685e3889dbfab8c963b
Dányi Bence <madbence@gmail.com>
2014-01-04 16:51:47 +0100

    Message parameter is optional

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports.encode = function encode(type, message) {
   if(type == null) {
     throw new Error('Type cannot be empty');
   }
+  message = message || '';
   var b = new Buffer(14 + message.length);
   b.write('i3-ipc');
   b.writeUInt32BE(message.length, 6);
