aee2eb4e1b70ea4580e3bc01e5a730e818cad191
Dányi Bence <madbence@gmail.com>
2014-01-04 16:44:04 +0100

    Returned Buffer has correct length

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports.encode = function encode(type, message) {
   if(type == null) {
     throw new Error('Type cannot be empty');
   }
-  var b = new Buffer('i3-ipcxxxxyyyy');
+  var b = new Buffer(14 + message.length);
+  b.write('i3-ipc');
   b.writeUInt32BE(type, 10);
   return b;
 };
