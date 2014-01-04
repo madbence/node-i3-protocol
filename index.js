c14fe4873208931aefffaa6161bff1b89c5a530a
Dányi Bence <madbence@gmail.com>
2014-01-04 17:04:21 +0100

    Message length is written to the Buffer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports.encode = function encode(type, message) {
   }
   var b = new Buffer(14 + message.length);
   b.write('i3-ipc');
+  b.writeUInt32BE(message.length, 6);
   b.writeUInt32BE(type, 10);
   return b;
 };
